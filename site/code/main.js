312e868d4750e3830674d8af94c56138ba0015aa
EggieIV <eckert994@gmail.com>
2019-03-31 17:00:34 +0200

    uploaded to edit on portable machine

--- site/code/main.js
+++ site/code/main.js
@@ -34,5 +34,60 @@ function toggleDrawer(Element) {
         //document.getElementById(Element).style.display = 'none';
         document.getElementById(Element).style.visibility = "hidden";
     }
+    
+    
+    
+    var contents = [];
+    
+    for (var i = 0; document.getElementById(Element).childElementCount > i; i++){
+        var newElement = document.getElementById(Element).childNodes()
+    }
+    
+    
+    
 }
 
+
+
+
+function myFunction(Element) {
+  var c = document.getElementById(Element).childNodes;
+  var txt = "";
+  for (var i = 0; i < c.length; i++) {
+    txt = txt + c[i].nodeName + "<br>";
+  }
+
+alert(txt);
+}
+
+
+
+  //  var map = embed.getElementById("mybody");
+
+var slider1 = document.getElementById("viewingAngle");
+
+//var map = document.getElementById("map");
+    var embed = document.getElementById("myEmbed");
+
+
+slider1.onchange = function () {
+
+    // alert(slider1.value);
+    //var element=embed.document.getElementById("myEmbed"); //findet element nicht 
+
+
+    //embed.style.webkitTransform ="rotateZ(45deg)";kinda working ?....
+
+    //document.getElementById("map").rotateX(slider1.value);
+    //alert ('this is '+slider1.value+ ' deg');
+    // embed.style.webkitTransform ="rotateZ(" +slider1.value+ "deg)";
+
+
+
+   let map= embed.innerHTML;
+
+
+
+}
+
+var slider2 = document.querySelector("#centerRotation");
